f28d9bffa3a8127e39959631179772b17ba65fe5
thisnamemeansnothing <thisnamemeansnothing@91da9267-612d-ac04-6195-048983e21811>
2011-11-03 09:31:53 +0000

    remoting

---
+++ OperatingManagement/RemotingObjectEntity/Account.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OperatingManagement.RemotingObjectInterface;
+using OperatingManagement.DataAccessLayer.System;
+using OperatingManagement.Framework;
+using System.Xml.Linq;
+
+namespace OperatingManagement.RemotingObjectEntity
+{
+    public class Account:MarshalByRefObject, IAccount
+    {
+        public string ValidateUser(string userName, string password)
+        {
+            XElement root = new XElement("status");
+            XElement code = new XElement("code");
+            XElement msg = new XElement("msg");
+            XElement user = new XElement("user");
+            XElement roles = new XElement("roles");
+            XElement permissions = new XElement("permissions");
+
+            try
+            {
+                User u = new User()
+                {
+                    LoginName = userName,
+                    Password = password
+                };
+                FieldVerifyResult retVal = u.Verify();
+                switch (retVal)
+                {
+                    case FieldVerifyResult.NotExist:
+                        code.Value = "1";
+                        break;
+                    case FieldVerifyResult.PasswordIncorrect:
+                        code.Value = "2";
+                        break;
+                    case FieldVerifyResult.Inactive:
+                        code.Value = "3";
+                        break;
+                    case FieldVerifyResult.Error:
+                        code.Value = "4";
+                        break;
+                    case FieldVerifyResult.Success:
+                        code.Value = "5";
+                        user.Add(new XElement("id", u.Id),
+                            new XElement("displayName", u.DisplayName),
+                            new XElement("LoginName", u.LoginName),
+                            new XElement("mobile", u.Mobile),
+                            new XElement("state", u.Status),
+                            new XElement("userType", u.UserType),
+                            new XElement("createdTime", u.CreatedTime.ToString("yyyyMMdd HH:mm:ss",
+                                System.Globalization.DateTimeFormatInfo.InvariantInfo)));
+                        var rs = u.SelectRolesById();
+                        if (rs != null && rs.Count > 0)
+                        {
+                            foreach (var r in rs) {
+                                roles.Add(new XElement("role", 
+                                    new XElement("id", r.Id),
+                                    new XElement("name", r.RoleName)));
+                            }
+                        }
+                        var p = new Permission();
+                        var ps = p.SelectByLoginName(userName);
+                        if (ps != null && ps.Count > 0)
+                        {
+                            foreach (var per in ps)
+                            {
+                                permissions.Add(new XElement("permission",
+                                    new XElement("id", per.Id),
+                                    new XElement("module",
+                                        new XElement("id", per.Id),
+                                        new XElement("name", per.Module.ModuleName)),
+                                    new XElement("task",
+                                        new XElement("id", per.Task.Id),
+                                        new XElement("name", per.Task.TaskName)))
+                                        );
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                code.Value = "0";
+                msg.SetValue(new XCData(ex.Message));
+            }
+            root.Add(code, msg, roles, permissions);
+            return root.ToString();
+        }
+    }
+}
---
+++ OperatingManagement/RemotingObjectInterface/IAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperatingManagement.RemotingObjectInterface
+{
+    /// <summary>
+    /// Public interfaces for Account management.
+    /// </summary>
+    public interface IAccount
+    {
+        /// <summary>
+        /// This method is used to validate user status by user name and password, and returns 
+        /// with the formated XML Data.
+        /// </summary>
+        /// <param name="userName">The name(LoginName) of user which to be validated.</param>
+        /// <param name="password">The password of user which to be validated.</param>
+        /// <returns></returns>
+        string ValidateUser(string userName, string password);
+    }
+}
