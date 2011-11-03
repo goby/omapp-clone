b76a98b87e0a157fa9e42997cffe63798334c9e9
thisnamemeansnothing <thisnamemeansnothing@91da9267-612d-ac04-6195-048983e21811>
2011-11-03 09:31:53 +0000

    remoting

---
+++ trunk/OperatingManagement/RemotingClientConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OperatingManagement.RemotingObjectInterface;
+
+namespace RemotingClientConsole
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            IAccount account = OperatingManagement.RemotingClient.RemotingActivator.GetObject<IAccount>();
+            Console.WriteLine(account.ValidateUser("admin", "123456"));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ trunk/OperatingManagement/RemotingServerConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemotingServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starts Remoting Server...");
+            OperatingManagement.RemotingServer.RemotingServerManager.Instance.Start();
+            Console.WriteLine("Remoting Server was started.");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
